8ae44f5525851d068af2d5b28553340d9bedea2e
NandaAdisaputra <nandaadisaputra18@gmail.com>
2018-12-23 12:07:04 +0700

    first commit

---
+++ app/src/main/java/com/nandaadisaputra/master/navigasi_master/CallFragment.kt
@@ -0,0 +1,32 @@
+package com.nandaadisaputra.master.navigasi_master
+
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CallFragment : Fragment() {
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabs: TabLayout
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view: View = inflater.inflate(R.layout.fragment_call, container, false)
+        viewPager = view.findViewById(R.id.viewpager_main)
+        tabs = view.findViewById(R.id.tabs_main)
+
+        val fragmentAdapter = MyPagerAdapter(childFragmentManager)
+        viewPager.adapter = fragmentAdapter
+        tabs.setupWithViewPager(viewPager)
+
+        return view
+    }
+
+}// Required empty public constructor
---
+++ app/src/main/java/com/nandaadisaputra/master/navigasi_master/MyPagerAdapter.kt
@@ -0,0 +1,32 @@
+package com.nandaadisaputra.master.navigasi_master
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                VoiceCallFragment()
+            }
+            else -> {
+                return VideoCallFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Voice Call"
+            else -> {
+                return "Video Call"
+            }
+        }
+    }
+}
